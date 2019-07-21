9a432df4fb2ab58fcbc6439a058835dd084b3d7e
ShitGameDev <ShitGameDev>
2019-07-21 18:36:12 -0400

    convert indentions

--- Project/Player/playercharacter.gd
+++ Project/Player/playercharacter.gd
@@ -6,24 +6,24 @@ const WALK_SPEED = 200
 var velocity = Vector2()
 
 func _physics_process(delta):
-    velocity.y += delta * GRAVITY
+	velocity.y += delta * GRAVITY
 
-    if Input.is_action_pressed("ui_left"):
-        velocity.x = -WALK_SPEED
-        $Sprite.flip_h = false
-    elif Input.is_action_pressed("ui_right"):
-        velocity.x =  WALK_SPEED
-        $Sprite.flip_h = true
-    else:
-        velocity.x = 0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+		$Sprite.flip_h = true
+	else:
+		velocity.x = 0
 
-    if abs(velocity.x) > 0:
-        $AnimationPlayer.play("walk")
-    else:
-        $AnimationPlayer.play("idle")
+	if abs(velocity.x) > 0:
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("idle")
 
-    # We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
+	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 
-    # The second parameter of "move_and_slide" is the normal pointing up.
-    # In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-    move_and_slide(velocity, Vector2(0, -1))
+	# The second parameter of "move_and_slide" is the normal pointing up.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity, Vector2(0, -1))
